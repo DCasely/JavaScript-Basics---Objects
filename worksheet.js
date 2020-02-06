03436362376776a6f72fd735feeb05cc0e9ae03e
DCASELY <24211985+DCasely@users.noreply.github.com>
2020-02-06 13:32:33 -0500

    covered Arrays vs Objects

---
+++ worksheet.js
@@ -0,0 +1,59 @@
+// var person = {
+//   name: 'Cindy',
+//   age: 32,
+//   city: 'Missoula'
+// };
+// var person2 = {
+//   name: 'Travis',
+//   age: 21,
+//   city: 'LA'
+// };
+
+// // UPDATING DATA IN OBJECTS
+// person2.age++;
+// person['city'] = 'London';
+
+// console.log(person, person2);
+
+// // BRACKET NOTATION
+// console.log(person['name']);
+
+// // DOT NOTATION
+// console.log(person.name);
+
+// var dog = {
+//   name: 'Rusty',
+//   breed: 'Mutt',
+//   age: 3
+// };
+
+// console.log(dog);
+
+// var str = name;
+// console.log(dog.str);
+// console.log(dog['str']);
+
+// ARRAYS VS OBJECTS
+// =========================================
+
+// array
+var dogs = ['Rusty', 'Lucky', 'Bubba'];
+console.log(dogs);
+console.log(dogs[1]);
+dogs.push('Wyatt');
+console.log(dogs);
+console.log(dogs[1]); // Lucky
+dogs[1] = 'Lucy'; // Reassigned to Lucy
+console.log(dogs);
+
+// object
+var dog = {
+  name: 'Bubba',
+  breed: 'Lab'
+};
+console.log(dog);
+console.log(dog['name']); // console.log(dog.name);
+dog['age'] = 6; // dog.age = 6;
+console.log(dog);
+dog.breed = 'Black Lab'; // dog['breed'] = 'Black Lab';
+console.log(dog);
